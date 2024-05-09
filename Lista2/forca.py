74de5e62e93996762b8d6ea567d327870bc726fb
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-05-08 21:51:15 -0300

    MUDA

--- Lista2/forca.py
+++ Lista2/forca.py
@@ -1,15 +1,20 @@
 qtd = int(input())
 
+
+
 for i in range(qtd):
     palavra = input()
 
     parte = ""
 
     for i in range(len(palavra)):
-        parte += "_"
+        if palavra[i] == " ":
+            parte += palavra[i]
+        else:
+            parte += "_"
 
     print(parte)
-
+    ultima = parte
     while parte != palavra:
         chute = input()
         parte = ""
@@ -17,8 +22,14 @@ for i in range(qtd):
             
             if chute == palavra[i]:
                 parte += chute
+            elif chute != ultima[i]:
+                parte += ultima[i]
+            elif " " == palavra[i]:
+                parte += " "
             else: 
                 parte += "_"
+
+        ultima = parte
 
         print(parte)
 
