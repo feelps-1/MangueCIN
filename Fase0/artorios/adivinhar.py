fbe859f0298781d1b96dfcb98de22e33afa0da67
Artur-Muad-dib <artur.aam@ufpe.br>
2024-05-14 10:10:54 -0300

    ainda não funciona aaaaaaaaaaaa

--- Fase0/artorios/adivinhar.py
+++ Fase0/artorios/adivinhar.py
@@ -1,5 +1,59 @@
 amigos = int(input())
 chutes = list(map(int, input().split()))
-diferenca = list(map(int, input().split()))
+erro = list(map(int, input().split()))
 
-print(chutes)
+chutes = sorted(chutes)
+erro = sorted(erro)
+
+menorChute = chutes[0]
+maiorCHute = chutes[-1]
+menorErro = erro[0]
+maiorErro = erro[-1]
+possibilidades = [
+    menorChute - menorErro,
+    menorChute + menorErro,
+    menorChute - maiorErro,
+    menorChute + maiorErro,
+    maiorCHute - menorErro,
+    maiorCHute + menorErro,
+    maiorCHute - maiorErro,
+    maiorCHute + maiorErro,
+]
+
+
+resultado = []
+
+
+class BreakIt(Exception):
+    pass
+
+
+try:
+    for i in range(len(possibilidades)):
+        totalErro = []
+        for j in range(len(chutes)):
+            momento = abs(possibilidades[i] - chutes[j])
+            if momento in erro:
+                totalErro.append(momento)
+                if len(totalErro) == len(erro) and possibilidades[i] not in resultado:
+                    resultado.append(possibilidades[i])
+                    break
+            else:
+                break
+
+
+except BreakIt:
+    pass
+
+
+
+
+resultado = sorted(resultado)
+
+resultado = [x for x in resultado if x > 0 and x < 10**9]
+
+print(resultado[0]) if resultado[0] > 0 else None
+
+if len(resultado) > 1:
+    resultado = sorted(resultado)
+    print(resultado[1])
