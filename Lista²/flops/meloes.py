e65c8a4b4d461edf2a3f56de3109dbad06eb05de
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-05-24 19:44:47 -0300

    Update meloes.py

--- Lista²/flops/meloes.py
+++ Lista²/flops/meloes.py
@@ -1,20 +1,20 @@
 info = [["Torre do Mago", "Rancho da Marnie", "Saloon",
-         "Armazém do Pierre", "Casa do Prefeito", "Peixaria",
-         "Museu", "Ferreiro", "Mercado Joja",
-         "Carpintaria", "Minas", "Centro Comunitário"],
-        [700, 260, 1200,
-         1100, 1500, 1900,
-         1870, 1700, 1800,
-         1500, 1850, 1300],
-        ["06:00", "09:00", "12:00",
-         "09:00", "08:30", "09:00",
-         "08:00", "09:00", "09:00",
-         "09:00", "Todo o dia", "Todo o dia"],
-        ["23:00", "16:00", "24:00",
-         "17:00", "22:00", "17:00",
-         "18:00", "16:00", "23:00",
-         "17:00", "Todo o dia", "Todo o dia"]
-        ]
+          "Armazém do Pierre", "Casa do Prefeito", "Peixaria",
+          "Museu", "Ferreiro", "Mercado Joja",
+          "Carpintaria", "Minas", "Centro Comunitário"],
+          [700, 260, 1200,
+           1100, 1500, 1900,
+           1870, 1700, 1800,
+           1500, 1850, 1300],
+          ["06:00", "09:00", "12:00",
+           "09:00", "08:30", "09:00",
+           "08:00", "09:00", "09:00",
+           "09:00", "Todo o dia", "Todo o dia"],
+          ["23:00", "16:00", "24:00",
+           "17:00", "22:00", "17:00",
+           "18:00", "16:00", "23:00",
+           "17:00", "Todo o dia", "Todo o dia"]
+          ]
 
 alibis = []
 ladrao = []
@@ -37,10 +37,14 @@ while not acabou:
 
         todosLadroes = ", ".join(ladrao)
         todosLocais = ", ".join(mentiraLocal)
-
-        print(f"{todosLocais} nem existem! {todosLadroes} roubaram os melões!")
+        
+        if len(ladrao) == 1:
+          print(f"Esse lugar {todosLocais} nem existe! {todosLadroes} foi quem roubou os melões!")
+        else:
+          print(f"{todosLocais} nem existem! {todosLadroes} roubaram os melões!")
+          
         acabou = True
-    else:  # horarios
+    else: #horarios
         for j in alibis:
             if info[2][info[0].index(j[2])] != "Todo o dia":
                 horarioAlibi = [int(z) for z in j[1].split(":")]
@@ -64,14 +68,16 @@ while not acabou:
                         mentiraLocal.append(j[2])
 
         if ladrao != []:
-
+            
             if len(ladrao) == 1:
                 fechamento = info[3][info[0].index(mentiraLocal[0])]
                 if fechamento == "24:00":
                     fechamento = "00:00"
+                    
+                todosLadroes = ", ".join(ladrao)
+                todosLocais = ", ".join(mentiraLocal)
 
-                print(
-                    f"{mentiraLocal} nem estava aberto nessa hora, esse lugar foi fechado às {fechamento}! {ladrao} roubou os melões!")
+                print(f"{todosLocais} nem estava aberto nessa hora, esse lugar foi fechado às {fechamento}! {todosLadroes} roubou os melões!")
             else:
                 ladrao = sorted(ladrao)
                 mentiraLocal = sorted((mentiraLocal))
@@ -79,8 +85,7 @@ while not acabou:
                 todosLadroes = ", ".join(ladrao)
                 todosLocais = ", ".join(mentiraLocal)
 
-                print(
-                    f"{todosLocais} nem estavam abertos nessa hora, esses lugares foram fechados beeem antes! {todosLadroes} se uniram e roubaram os melões!")
+                print(f"{todosLocais} nem estavam abertos nessa hora, esses lugares foram fechados beeem antes! {todosLadroes} se uniram e roubaram os melões!")
 
             acabou = True
         else:
@@ -100,11 +105,9 @@ while not acabou:
             acabou = True
 
             if len(ladrao) == 1:
-                print(
-                    f"{todosLadroes} estava a {distancias[0]} metros da plantação! Agora é nosso suspeito número um. Fiquem de olho!")
+                print(f"{todosLadroes} estava a {distancias[0]} metros da plantação! Agora é nosso suspeito número um. Fiquem de olho!")
             else:
-                print(
-                    f"{todosLadroes} estavam a {distancias[0]} metros da plantação! Eles podiam estar cometendo o roubo juntos... Fiquem de olho!")
+                print(f"{todosLadroes} estavam a {distancias[0]} metros da plantação! Eles podiam estar cometendo o roubo juntos... Fiquem de olho!")
 
 
 
